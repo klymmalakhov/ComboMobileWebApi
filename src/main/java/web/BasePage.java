4e7cd278bf4da4daf7960d24c015bd381e5dfd07
kmalakhov <klyment.malakhov@exness.com>
2020-01-14 17:13:40 +0200

    [General]: Add log4j for base action

--- src/main/java/web/BasePage.java
+++ src/main/java/web/BasePage.java
@@ -1,10 +1,17 @@
 package web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BasePage {
 
+    private final Logger log = LoggerFactory.getLogger(BasePage.class);
+
     public void openBrowser(String browserName) {
+        log.info("Starting the {} browser", browserName);
     }
 
     public void openPage(String pageName) {
+        log.info("Open the {} page", pageName);
     }
 }
